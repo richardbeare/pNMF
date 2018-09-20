0becfe4f5c9e7c6f5b0ec0130a19d29963bfd195
Richard Beare <Richard.Beare@gmail.com>
2018-09-20 22:34:13 +1000

    A version that runs

--- R/pnmf.R
+++ R/pnmf.R
@@ -5,18 +5,27 @@
 #' @param nmfMod NMF model from the NMF package
 #' @param tol tolerance for stopping criteria
 #' @param maxIter Maximum number of iterations
-#' @param checkDivergence
-#'
+#' @param verbose Print status messages
 #' @return Fitted NMF model, as defined in NMF package.
 #' @export
 #'
 #' @importFrom NMF basis coef
 #' @examples
-#' NMF::register(PNMF)
-#'
-PNMF <- function (X, nmfMod, tol = 1e-5, maxIter = 500, checkDivergence = TRUE) {
+#' setNMFMethod("PNMF", pNMF::PNMF)
+#' mkD <- function(NOISE=TRUE) {
+#'   n <- 1000 # rows
+#'   counts <- c(30, 10, 20, 10, 15, 15) # samples
+#'   syntheticNMF(n=n, r=counts, offset = NULL, noise = NOISE,
+#'                factors = FALSE, seed = 99)
+#' }
+#' k<-mkD()
+#' V.random <- randomize(k)
+#' estim.r2 <- nmf(k, 2:20, method="PNMF", nrun=30)
+#' estim.r2.random <- nmf(V.random, 2:20,  method="PNMF", nrun=30)
+
+PNMF <- function (X, nmfMod, tol = 1e-5, maxIter = 500, verbose=FALSE) {
   # Initialization
-  startTime <- proc.time()[3]
+  #startTime <- proc.time()[3]
   W <- NMF::basis(nmfMod)
   #H <- NMF::coef(nmfMod)
   err <- rep(0, times = maxIter+1)
@@ -26,7 +35,6 @@ PNMF <- function (X, nmfMod, tol = 1e-5, maxIter = 500, checkDivergence = TRUE) 
   W0 <- W
   hasDiverged <- FALSE
   XX <- tcrossprod(X)
-
   for (iter in 1:maxIter) {
     W_old <- W
     # matlab stuff
@@ -35,13 +43,16 @@ PNMF <- function (X, nmfMod, tol = 1e-5, maxIter = 500, checkDivergence = TRUE) 
     #diffW = norm(W_old-W, 'fro') / norm(W_old, 'fro');
     W <- W * (XX %*% W)/ (W %*% (crossprod(W, XX)%*%W) + XX %*% W %*% crossprod(W))
     W <- W/norm(W, "2")
-    diffW <- norm(W_old-W, 'fro') / norm(W_old, 'fro')
+    diffW <- norm(W_old-W, 'F') / norm(W_old, 'F')
     if (diffW < tol) {
+      if (verbose) {
+        cat("Convergence in ", iter, " iterations\n")
+      }
       break
     }
   }
   gc()
   NMF::basis(nmfMod) <- W
-  NMF::coef(nmfMod) <- t(W)
+  NMF::coef(nmfMod) <- crossprod(W, X)
   return(nmfMod)
 }
