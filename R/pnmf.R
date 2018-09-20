498d6d34464c37cc7784aa6195888d818b5b9668
Richard Beare <richard.beare@monash.edu>
2018-09-20 16:56:47 +1000

    beginning

---
+++ R/pnmf.R
@@ -0,0 +1,17 @@
+
+#' Projective nonnegative matrix factorization
+#'
+#' @param X
+#' @param nmfMod
+#' @param tol
+#' @param maxIter
+#' @param timeLimit
+#' @param checkDivergence
+#'
+#' @return
+#' @export
+#'
+#' @examples
+PNMF <- function (X, nmfMod, tol = 1e-5, maxIter = 500, timeLimit = 300, checkDivergence = TRUE) {
+
+}
