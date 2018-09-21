4166950d86e95157854d9cc8d229f3ebfbb7fd99
Richard Beare <Richard.Beare@gmail.com>
2018-09-21 13:40:24 +1000

    added a face dataset

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Face data base from the matlab pnmf package
+#'
+#' Originally from the FERET database, https://www.itl.nist.gov/iad/humanid/feret/.
+#'
+#' @format Faces are 32x32 images, dataset is 2049 rows,
+#' 1024 (=32x32) columns
+"faces"
