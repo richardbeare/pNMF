2acc32389754f04ccbae4aa4006308f4634c1444
Richard Beare <Richard.Beare@gmail.com>
2018-09-27 09:54:33 +1000

    further testing

--- pnmf_face_trials.R
+++ pnmf_face_trials.R
@@ -33,6 +33,12 @@ faces.brunet.nndsvd <- nmf(t(faces), rank = trialrank, method='brunet', seed="nn
 ## ---- BrunetRandomSeed ----
 faces.brunet.random <- nmf(t(faces), rank = trialrank, method='brunet', seed="random", nrun=20 )
 
+## ---- Offset ----
+faces.offset.ica <- nmf(t(faces), rank = trialrank, method='offset', seed="ica" )
+faces.offset.nndsvd <- nmf(t(faces), rank = trialrank, method='offset', seed="nndsvd" )
+
+faces.offset.random <- nmf(t(faces), rank = trialrank, method='offset', seed="random", nrun=20 )
+
 ## ---- BrunetPlotICA ----
 faces.basis.brunet <- NMF::basis(faces.brunet.ica)
 
@@ -56,3 +62,48 @@ ggfbb <- NMFimage2df(t(faces.basis.brunet), 32,32)
 ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
   geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
   coord_fixed()
+
+## ---- OffsetPlotICA ----
+faces.basis.offset <- NMF::basis(faces.offset.ica)
+
+ggfbb <- NMFimage2df(t(faces.basis.offset), 32,32)
+ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
+  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
+  coord_fixed()
+
+## ---- OffsetPlotNNDSVD ----
+faces.basis.offset <- NMF::basis(faces.offset.nndsvd)
+
+ggfbb <- NMFimage2df(t(faces.basis.offset), 32,32)
+ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
+  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
+  coord_fixed()
+
+## ---- OffsetPlotRandom ----
+faces.basis.offset <- NMF::basis(faces.offset.random)
+
+ggfbb <- NMFimage2df(t(faces.basis.offset), 32,32)
+ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
+  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
+  coord_fixed()
+
+
+## ---- Projective ----
+faces.pnmf.ica <- nmf(t(faces), rank = trialrank, method='PNMF', seed="ica" )
+faces.pnmf.nndsvd <- nmf(t(faces), rank = trialrank, method='PNMF', seed="nndsvd" )
+
+faces.pnmf.random <- nmf(t(faces), rank = trialrank, method='PNMF', seed="random", nrun=20 )
+
+## ---- ProjectiveOrtho ----
+faces.pnmfo.ica <- nmf(t(faces), rank = trialrank, method='PNMFO', seed="ica" )
+faces.pnmfo.nndsvd <- nmf(t(faces), rank = trialrank, method='PNMFO', seed="nndsvd" )
+
+faces.pnmfo.random <- nmf(t(faces), rank = trialrank, method='PNMFO', seed="random", nrun=20 )
+
+## ---- ProjectiveKL ----
+faces.pnmfkl.ica <- nmf(t(faces), rank = trialrank, method='PNMFKL', seed="ica" )
+faces.pnmfkl.nndsvd <- nmf(t(faces), rank = trialrank, method='PNMFKL', seed="nndsvd" )
+
+faces.pnmfkl.random <- nmf(t(faces), rank = trialrank, method='PNMFKL', seed="random", nrun=20 )
+
+## ---- Last ----
