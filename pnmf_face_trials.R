36793435f648e37018977b1895c01d29b989158f
Richard Beare <Richard.Beare@gmail.com>
2018-10-04 11:13:13 +1000

    faces trials complete

--- pnmf_face_trials.R
+++ pnmf_face_trials.R
@@ -40,28 +40,20 @@ faces.offset.nndsvd <- nmf(t(faces), rank = trialrank, method='offset', seed="nn
 faces.offset.random <- nmf(t(faces), rank = trialrank, method='offset', seed="random", nrun=20 )
 
 ## ---- BrunetPlotICA ----
-faces.basis.brunet <- NMF::basis(faces.brunet.ica)
+plotNMFBasis <- function(ff) {
+  faces.basis.brunet <- NMF::basis(ff)
 
-ggfbb <- NMFimage2df(t(faces.basis.brunet), 32,32)
-ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
-  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
-  coord_fixed()
-
+  ggfbb <- NMFimage2df(t(faces.basis.brunet), 32,32)
+  ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
+    geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
+    coord_fixed()
+}
+plotNMFBasis(faces.brunet.ica)
 ## ---- BrunetPlotNNDSVD ----
-faces.basis.brunet <- NMF::basis(faces.brunet.nndsvd)
-
-ggfbb <- NMFimage2df(t(faces.basis.brunet), 32,32)
-ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
-  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
-  coord_fixed()
+plotNMFBasis(faces.brunet.nndsvd)
 
 ## ---- BrunetPlotRandom ----
-faces.basis.brunet <- NMF::basis(faces.brunet.random)
-
-ggfbb <- NMFimage2df(t(faces.basis.brunet), 32,32)
-ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
-  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
-  coord_fixed()
+plotNMFBasis(faces.brunet.random)
 
 ## ---- OffsetPlotICA ----
 faces.basis.offset <- NMF::basis(faces.offset.ica)
@@ -72,21 +64,10 @@ ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
   coord_fixed()
 
 ## ---- OffsetPlotNNDSVD ----
-faces.basis.offset <- NMF::basis(faces.offset.nndsvd)
-
-ggfbb <- NMFimage2df(t(faces.basis.offset), 32,32)
-ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
-  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
-  coord_fixed()
+plotNMFBasis(faces.offset.nndsvd)
 
 ## ---- OffsetPlotRandom ----
-faces.basis.offset <- NMF::basis(faces.offset.random)
-
-ggfbb <- NMFimage2df(t(faces.basis.offset), 32,32)
-ggplot(ggfbb, aes(x=Var2, y=33-Var1, fill=Brightness)) +
-  geom_tile() + facet_wrap(~ID) + scale_fill_gradient(low="black", high="white") +
-  coord_fixed()
-
+plotNMFBasis(faces.offset.random)
 
 ## ---- Projective ----
 faces.pnmf.ica <- nmf(t(faces), rank = trialrank, method='PNMF', seed="ica" )
@@ -105,5 +86,85 @@ faces.pnmfkl.ica <- nmf(t(faces), rank = trialrank, method='PNMFKL', seed="ica" 
 faces.pnmfkl.nndsvd <- nmf(t(faces), rank = trialrank, method='PNMFKL', seed="nndsvd" )
 
 faces.pnmfkl.random <- nmf(t(faces), rank = trialrank, method='PNMFKL', seed="random", nrun=20 )
+
+
+## ---- ProjectivePlotICA ----
+plotNMFBasis(faces.pnmf.ica)
+
+## ---- ProjectivePlotNNDSVD ----
+plotNMFBasis(faces.pnmf.nndsvd)
+
+## ---- ProjectivePlotRandom ----
+plotNMFBasis(faces.pnmf.random)
+
+## ---- ProjectiveOPlotICA ----
+plotNMFBasis(faces.pnmfo.ica)
+
+## ---- ProjectiveOPlotNNDSVD ----
+plotNMFBasis(faces.pnmfo.nndsvd)
+
+## ---- ProjectiveOPlotRandom ----
+plotNMFBasis(faces.pnmfo.random)
+
+## ---- ProjectiveKLPlotICA ----
+plotNMFBasis(faces.pnmfkl.ica)
+
+## ---- ProjectiveKLPlotNNDSVD ----
+plotNMFBasis(faces.pnmfkl.nndsvd)
+
+## ---- ProjectiveKLPlotRandom ----
+plotNMFBasis(faces.pnmfkl.random)
+
+## ---- PNMFAveSVD ----
+faces.pnmfo.nndsvdA <- nmf(t(faces), rank = trialrank,
+                           method='PNMFO',
+                           seed=list(method="nndsvd", densify="average"))
+faces.pnmf.nndsvdA <- nmf(t(faces), rank = trialrank,
+                         method='PNMF',
+                         seed=list(method="nndsvd", densify="average") )
+
+## ---- PlotPNMFAveSVD ----
+plotNMFBasis(faces.pnmfo.nndsvdA)
+plotNMFBasis(faces.pnmf.nndsvdA)
+
+## ---- RankSelectPNM ----
+ff <- "rsel.Rda"
+if (file.exists(ff)) {
+  load(ff)
+} else {
+  oo <- list(parallel=10, verbose=TRUE)
+  faces.pnmfo.nndsvdA.rs <- nmf(t(faces), rank = 2:50,
+                                method='PNMFO',
+                                seed=list(method="nndsvd", densify="average"), nrun=1,
+                                .pbackend="mc", .options=oo)
+  faces.pnmf.nndsvdA.rs <- nmf(t(faces), rank = 2:50,
+                               method='PNMF',
+                               seed=list(method="nndsvd", densify="average"),nrun=1,
+                               .pbackend="mc", .options=oo)
+  save(faces.pnmfo.nndsvdA.rs, faces.pnmf.nndsvdA.rs, file=ff)
+}
+ff <- "rselrandom.Rda"
+if (file.exists(ff)) {
+  load(ff)
+} else {
+  tfacesrand <- randomize(t(faces))
+
+  oo <- list(parallel=10, verbose=TRUE)
+  faces.pnmfo.nndsvdA.rs.rand <- nmf(tfacesrand, rank = 2:50,
+                                method='PNMFO',
+                                seed=list(method="nndsvd", densify="average"), nrun=1,
+                                .pbackend="mc", .options=oo)
+  faces.pnmf.nndsvdA.rs.rand <- nmf(tfacesrand, rank = 2:50,
+                               method='PNMF',
+                               seed=list(method="nndsvd", densify="average"),nrun=1,
+                               .pbackend="mc", .options=oo)
+  save(faces.pnmfo.nndsvdA.rs.rand, faces.pnmf.nndsvdA.rs.rand, file=ff)
+}
+
+## ---- PlotRankPNMFO ----
+rss.rankplot(faces.pnmfo.nndsvdA.rs, faces.pnmfo.nndsvdA.rs.rand)
+
+## ---- PlotRankPNMF ----
+rss.rankplot(faces.pnmf.nndsvdA.rs, faces.pnmf.nndsvdA.rs.rand)
 
 ## ---- Last ----
