c99fa712785599f31e089f723d9cdc64b41c39d4
Richard Beare <Richard.Beare@monash.edu>
2020-06-20 16:21:16 +1000

    more playing with benchmarking

--- R/ard.R
+++ R/ard.R
@@ -8,7 +8,36 @@ function() {
 
 
   k <- nmf(t(faces), rank = 64, method = "PNMFARD", seed = "random", verboseN = TRUE)
-}
+
+  W <- WW
+  dsigma <- diag(Sigma)
+  library(microbenchmark)
+  eps <- .Machine$double.eps
+  XX <- tcrossprod(t(faces))
+  microbenchmark({W1 <- W * ((XX %*% W) %*% dsigma / ((W %*% (t(W) %*% XX %*% W) + XX %*% W %*% (t(W) %*% W)) %*% dsigma + W + eps))}, times=100)
+  microbenchmark({W2 <- W * ((XX %*% W) %*% dsigma / ((W %*% (t(W) %*% XX %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma + W + eps))}, times=100)
+  microbenchmark({W3 <- W * ((XX %*% W) %*% dsigma / ((W %*% (crossprod(W, XX) %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma + W + eps))}, times=100)
+
+  all(W1 == W3)
+  max(abs(W1 - W3))
+  # slower!
+  #library(spam)
+  #dsigma2 <- diag.spam(Sigma)
+  #microbenchmark({W1 <- W * ((XX %*% W) %*% dsigma2 / ((W %*% (crossprod(W, XX) %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma2 + W + eps))}, times=100)
+
+  # create a special version of dsigma that gives the same answer via element-wise multiplication
+  dsigma3 <- matrix(rep(Sigma, rep(nrow(XX), length(Sigma))), ncol=length(Sigma))
+  all( ((XX %*% W) %*% dsigma) == ((XX %*% W) * dsigma3))
+  microbenchmark({W1 <- W * ((XX %*% W) * dsigma3 / ((W %*% (crossprod(W, XX) %*% W) + XX %*% W %*% crossprod(W)) * dsigma3 + W + eps))}, times=100)
+  # No difference, and that doesn't include generating sigma3
+  library(Matrix)
+  dsigma3 <- Diagonal(x=Sigma)
+  microbenchmark({W3 <- W * ((XX %*% W) %*% dsigma3 / ((W %*% (crossprod(W, XX) %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma3 + W + eps))}, times=100)
+  XXM <- Matrix(XX)
+  WM <- Matrix(W)
+  microbenchmark({W3 <- WM * ((XXM %*% WM) %*% dsigma3 / ((WM %*% (crossprod(WM, XXM) %*% WM) + XXM %*% WM %*% crossprod(WM)) %*% dsigma3 + WM + eps))}, times=100)
+  # Nope - slower. No point messing with the sigma assuming that it stays relatively small
+  }
 
 
 #' @export
@@ -23,14 +52,10 @@ PNMFARD <-function(X, nmfMod, tol = 1e-5, maxIter = 5000, verboseN=FALSE, zeroto
   for (iter in 1:maxIter) {
     W_old <- W
     sigma <- colSums(W * W)
-    WW <<- W
-    Sigma <<- sigma
     diag(dsigma) <- sigma
-    break
-#browser()
-        W <- W * ((XX %*% W) %*% dsigma / ((W %*% (t(W) %*% XX %*% W) + XX %*% W %*% (t(W) %*% W)) %*% dsigma + W + eps))
+    # W <- W * ((XX %*% W) %*% dsigma / ((W %*% (t(W) %*% XX %*% W) + XX %*% W %*% (t(W) %*% W)) %*% dsigma + W + eps))
     #W <- W * ((XX %*% W) %*% dsigma / ((W %*% (t(W) %*% XX %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma + W + eps))
-    #W <- W * ((XX %*% W) %*% dsigma / ((W %*% (crossprod(W, XX) %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma + W + eps))
+    W <- W * ((XX %*% W) %*% dsigma / ((W %*% (crossprod(W, XX) %*% W) + XX %*% W %*% crossprod(W)) %*% dsigma + W + eps))
     W <- W/norm(W, type = "2")
     diffW <- norm(W_old-W, type = "F")/norm(W_old, type = "F")
     if (diffW < tol) {
@@ -56,6 +81,7 @@ PNMFARD <-function(X, nmfMod, tol = 1e-5, maxIter = 5000, verboseN=FALSE, zeroto
   NMF::basis(nmfMod) <- W
   NMF::coef(nmfMod) <- crossprod(W, X)
 #browser()
-
+  stuff <- list(sigma = sigma, wnorm = )
+  nmfMod@extra <- c(nmfMod@extra, stuff)
   return(nmfMod)
 }
